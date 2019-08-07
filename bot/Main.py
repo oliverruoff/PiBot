b3158aad3d4ab499a7b300ef7f13e4e5781271bc
Oliver Ruoff <oliver93ruoff@web.de>
2019-08-07 22:19:52 +0200

    added robot driving functionality

---
+++ bot/Main.py
@@ -0,0 +1,95 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+IN1 = 19
+IN2 = 13
+IN3 = 6
+IN4 = 5
+ENA = 26
+ENB = 11
+temp1 = 1
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+GPIO.output(IN1, GPIO.LOW)
+GPIO.output(IN2, GPIO.LOW)
+p_a = GPIO.PWM(ENA, 1000)
+p_a.start(0)
+p_b = GPIO.PWM(ENB, 1000)
+p_b.start(0)
+print("\n")
+print("The default speed & direction of motor is STOP & Forward.....")
+print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
+print("\n")
+
+while(1):
+
+    x = raw_input()
+
+    if x == 'r':
+        print("run")
+        if(temp1 == 1):
+            GPIO.output(IN1, GPIO.HIGH)
+            GPIO.output(IN2, GPIO.LOW)
+            GPIO.output(IN3, GPIO.HIGH)
+            GPIO.output(IN4, GPIO.LOW)
+            print("forward")
+            x = 'z'
+        else:
+            GPIO.output(IN1, GPIO.LOW)
+            GPIO.output(IN2, GPIO.HIGH)
+            GPIO.output(IN3, GPIO.LOW)
+            GPIO.output(IN4, GPIO.HIGH)
+            print("backward")
+            x = 'z'
+
+    elif x == 's':
+        print("stop")
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.LOW)
+        x = 'z'
+
+    elif x == 'f':
+        print("forward")
+        GPIO.output(IN1, GPIO.HIGH)
+        GPIO.output(IN2, GPIO.LOW)
+        GPIO.output(IN3, GPIO.HIGH)
+        GPIO.output(IN4, GPIO.LOW)
+        temp1 = 1
+        x = 'z'
+
+    elif x == 'b':
+        print("backward")
+        GPIO.output(IN1, GPIO.LOW)
+        GPIO.output(IN2, GPIO.HIGH)
+        GPIO.output(IN3, GPIO.LOW)
+        GPIO.output(IN4, GPIO.HIGH)
+        temp1 = 0
+        x = 'z'
+
+    elif x == 'l':
+        print("low")
+        p_a.ChangeDutyCycle(25)
+        x = 'z'
+
+    elif x == 'm':
+        print("medium")
+        p_a.ChangeDutyCycle(50)
+        x = 'z'
+
+    elif x == 'h':
+        print("high")
+        p_a.ChangeDutyCycle(75)
+        x = 'z'
+
+    elif x == 'e':
+        GPIO.cleanup()
+        break
+
+    else:
+        print("<<<  wrong data  >>>")
+        print("please enter the defined data to continue.....")
