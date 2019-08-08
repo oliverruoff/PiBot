71cafceb25538a3be21f946b236651f2028934b4
Oliver Ruoff <oliver93ruoff@web.de>
2019-08-08 22:30:11 +0200

    update imports

--- bot/usdar/detection.py
+++ bot/usdar/detection.py
@@ -1,8 +1,8 @@
 import RPi.GPIO as GPIO
 import math
 
-import stepper as st
-import ultrasonic as us
+from usdar import stepper as st
+from usdar import ultrasonic as us
 
 
 def get_coord(angle, distance):
