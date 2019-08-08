47b234931d81e42c8f8edb2addaae697d7def8ec
Oliver Ruoff <oliver93ruoff@web.de>
2019-08-08 23:34:29 +0200

    improve turning angle calc

--- bot/main.py
+++ bot/main.py
@@ -3,24 +3,13 @@ import math
 import motor_controls as mc
 from usdar import detection
 
-
-def dotproduct(v1, v2):
-    return sum((a*b) for a, b in zip(v1, v2))
-
-
-def length(v):
-    return math.sqrt(dotproduct(v, v))
-
-
-def angle(v1, v2):
-    return math.acos(dotproduct(v1, v2) / (length(v1) * length(v2)))
-
-
 if __name__ == "__main__":
     env_map = detection.scan_360()
     distances = [(x, y, math.sqrt(x*x+y*y)) for x, y in env_map]
     max_dist = distances[[i[2]
                           for i in distances].index(max([i[2]
                                                          for i in distances]))]
-    angle_to_turn = math.degrees(angle((0, 1), (max_dist[0], max_dist[1])))
+    angle_to_turn = math.degrees(math.atan2(max_dist[0], max_dist[1]))
+    if angle_to_turn < 0:
+        angle_to_turn = 360 + angle_to_turn
     print('I should turn now:', angle_to_turn, '°.')
